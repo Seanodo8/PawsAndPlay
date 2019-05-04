5017aac8df3d8277040db47d9a84a3ae336b2527
joura <martinezluisjorge86@gmail.com>
2019-05-04 09:11:13 -0400

    routes sent

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Paws and Play
-      </a>
-      <a className="navbar-brand" href="/login">
-        Profile
-      </a>
-      <a className="navbar-brand" href="/">
-        Log Out
-      </a>
-    </nav>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Paws And Play
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/profile"
+          className={window.location.pathname === "/profile" ? "nav-link active" : "nav-link"}
+        >
+          Profile
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/login"
+          className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
+        >
+          Log Out
+        </Link>
+      </li>
+    </ul>
   );
 }
+
+
 
 export default Nav;
---
+++ client/src/pages/Park.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import Nav from "../components/Nav";
+
+
+class Park extends Component{
+    render(){
+        return(
+        <div>    
+        
+        <button>Hi</button>
+        </div>
+        )
+    }
+
+}
+
+
+export default Park;
