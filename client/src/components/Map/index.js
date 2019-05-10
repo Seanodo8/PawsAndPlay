2d5907c383ef483f8c852b6e7eca73f2fb83301e
Sean ODonnell <odonnellsean9045@gmail.com>
2019-05-10 17:10:42 -0400

    markers and intial center fix

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -30,6 +30,36 @@ export class MapContainer extends Component {
     return (
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
         <Marker onClick={this.onMarkerClick} name={"current location"} />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Eagle Nest Park"}
+          position={{ lat: 28.5171, lng: -81.4497 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Bill Frederick Park"}
+          position={{ lat: 28.507, lng: -81.4824 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Willie Mays Park"}
+          position={{ lat: 28.5177, lng: -81.4379 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Pleasant Valley Park"}
+          position={{ lat: 28.5309, lng: -81.4423 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Orlo Vista Park"}
+          position={{ lat: 28.542, lng: -81.4663 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Gilbert McQueen Park"}
+          position={{ lat: 28.5329, lng: -81.4232 }}
+        />
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
